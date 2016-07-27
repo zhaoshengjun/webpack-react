94ccdb4a5f9bd0b71a325fed57ee46a0b909bafa
zhaoshengjun <zhao.shengjun@gmail.com>
2016-07-27 11:33:14 +1000

    refactoring reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,25 @@
-import { combineReducers } from 'redux'
-import todos, * as fromTodos from './todos'
+import {ADD_TODO, TOGGLE_TODO, RECEIVE_TODOS}  from '../actions';
+import { combineReducers } from 'redux';
+import byId, * as fromById from './byId';
+import createList, * as fromList from './createList';
 
-const todoApp = combineReducers({
-  todos
+const listByFilter = combineReducers({
+  all: createList('all'),
+  active: createList('active'),
+  completed: createList('completed')
 })
 
-export default todoApp;
+const todos = combineReducers({
+  byId,
+  listByFilter,
+});
 
-export const getVisibleTodos = (state, filter) =>
-  fromTodos.getVisibleTodos(state.todos, filter);
+export default todos;
+
+const getAllTodos = (state) =>
+  state.allIds.map(id => state.byId[id]);
+
+export const getVisibleTodos = (state, filter) => {
+  const ids = fromList.getIds( state.listByFilter[filter]);
+  return ids.map(id => fromById.getTodo(state.byId, id));
+};
